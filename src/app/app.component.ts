e9e017b6f056a16fe8e66944e6c7ab587700bf6d
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-05-17 11:54:32 +0100

    Added header compoment and view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 
+import { headerComponent } from './controllers/header/app.headerComponent';
+
 @Component({
   selector: 'my-app',
   templateUrl: './views/main/main.html',
 })
-export class AppComponent  { }
+export class AppComponent { }
